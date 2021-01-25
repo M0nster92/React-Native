8776ddb2c6e2734cba73b49426a2245223a3d914
fahim2077 <fahimalsafi@gmail.com>
2021-01-24 22:00:20 -0500

    env file

--- track/server/index.js
+++ track/server/index.js
@@ -1,17 +1,20 @@
 require("./models/User");
+require("./models/Track");
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const jwt = require('jsonwebtoken');
 const authRoutes = require("./routes/authRoutes");
 const requireAuth = require("./middlewares/requireAuth");
+const trackRoutes = require("./routes/trackRoutes");
+require('dotenv').config()
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use(authRoutes)
+app.use(authRoutes);
+app.use(trackRoutes);
 
-const mongoUri = "mongodb+srv://sami_92:sami_92@cluster0.dovo5.mongodb.net/track?retryWrites=true&w=majority"
+const mongoUri = process.env.CONNECTION_URI;
 
 mongoose.connect(mongoUri, {
     useNewUrlParser : true,
